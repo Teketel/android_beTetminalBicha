9fe34d0c9112e3accf9c1c111317f42f012e98f5
tsegaab <tsegbs@gmail.com>
2014-06-02 16:17:56 +0300

    update

---
+++ src/androidTest/java/com/teke/hellogradle/tests/StringTest.java
@@ -0,0 +1,28 @@
+package com.teke.hellogradle.tests;
+
+import android.test.AndroidTestCase;
+import com.teke.hellogradle.StringStuff;
+
+
+public class StringTest extends AndroidTestCase {
+
+    private StringStuff mString;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mString = new StringStuff();
+    }
+
+    public void testAdd() {
+        String[] result = mString.stringSwitch("Hello", "there");
+        assertTrue("Hello" == result[1]);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+        
+    }
+
+}
