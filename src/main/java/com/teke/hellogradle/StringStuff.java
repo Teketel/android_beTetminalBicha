ee7e340522caacdc835c4c2209463d3b0966fc27
tsegaab <tsegbs@gmail.com>
2014-06-02 16:20:43 +0300

    update

--- src/main/java/com/teke/hellogradle/StringStuff.java
+++ src/main/java/com/teke/hellogradle/StringStuff.java
@@ -5,7 +5,7 @@ package com.teke.hellogradle;
 public class StringStuff {
 
     public String[] stringSwitch(String s1, String s2) {
-        String[] stringArray = String[2];
+        String[] stringArray = new String[2];
         stringArray[0] = s2;
         stringArray[1] = s1;
 
